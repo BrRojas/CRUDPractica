0834a83c0384442906a72bbf12d0efffc6607fd4
Braian Rojas <braianrojas95@gmail.com>
2025-08-04 12:34:52 -0300

    feat: crear entidad User

---
+++ Logica/src/main/java/com/logica/logica/model/User.java
@@ -0,0 +1,48 @@
+package com.logica.logica.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String user;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String user, String password) {
+        this.id = id;
+        this.user = user;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
